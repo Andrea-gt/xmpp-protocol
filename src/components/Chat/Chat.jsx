5a953dfd98ea9269b1aa7ff5fa11ede9d12f8891
Andrea Ramírez <77802937+Andrea-gt@users.noreply.github.com>
2024-08-16 21:37:45 -0600

    feat: Chat Message Bar

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -9,10 +9,32 @@ import {
   MenuItem,
   Tooltip,
   IconButton,
+  TextField,
+  Button
 } from "@mui/material";
+
+import AttachFileIcon from'@mui/icons-material/AttachFile';
+import SendIcon from '@mui/icons-material/Send';
 
 const Chat = () => {
   const { palette } = useTheme();
+  const [message, setMessage] = useState('');
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Handle message submissionconsole.log('Message:', message);
+    console.log('File:', file);
+    // Reset input fields after submissionsetMessage('');
+    setFile(null);
+  };
 
   return (
     <Box
@@ -28,14 +50,52 @@ const Chat = () => {
     >
       <Box
         width="100%"
-        height="100%"
+        height="90%"
         sx ={{
-          background: 'rgba(0, 0, 0, 0.5)',
+          background: 'rgba(0, 0, 0, 0.6)',
         }}
-      >
-        ""
+      />
+      {/* Chat messages go here */}
+      <Box
+        display="flex"
+        flexDirection="row"
+        width="100%"
+        height="10%"
+        alignItems="center"
+        padding="0 0.8rem"
+        gap="10px"
+        sx ={{
+          backgroundColor: palette.grey[200]
+        }}>
+        <TextField
+          fullWidth
+          variant="outlined"
+          placeholder="Type your message..."
+          value={message}
+          size="small"
+          sx={{
+            backgroundColor: "#F6F6F6"
+          }}
+          onChange={handleMessageChange}
+        />
+        <input
+          accept="image/*,video/*,audio/*,.pdf,.doc,.docx"
+          style={{ display: 'none' }}
+          id="file-upload"
+          type="file"
+          onChange={handleFileChange}
+        />
+        <Tooltip title="Attach a file">
+          <IconButton component="label" htmlFor="file-upload">
+            <AttachFileIcon />
+          </IconButton>
+        </Tooltip>
+        <Button
+          variant="contained"
+          color="primary"
+          endIcon={<SendIcon />}
+          onClick={handleSubmit}>Send</Button>
       </Box>
-
     </Box>
   );
 };
